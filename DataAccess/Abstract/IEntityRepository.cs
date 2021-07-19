3ed8c0e857fc1c421e4b9901424bd8aea754fdfb
Burak Yunkul <byunkul23@hotmail.com>
2021-07-19 13:16:09 +0300

    19 Temmuz 2021 Entity Framework -Open Closed Principle-Db tabloları ve proje class bağlantısı.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+	//Generic Repository Design Pattern
+	//class: referans tip 
+	//Buradaki generic yapıda sadece IEntity ve ondan implement edilen classlar T değeri yerinde kullanılır.
+	//IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+	//new() : new'lenebilir olmalı.
+	public interface IEntityRepository<T> where T:class,IEntity,new()
+	{
+		//Generic Constraint
+		List<T> GetAll(Expression<Func<T,bool>> filter=null);
+		T Get(Expression<Func<T, bool>> filter = null);
+		void Add(T entity);
+		void Delete(T entity);
+		void Update(T entity);
+
+	}
+}
