1ac436bde360fcfeb0f1a3b1e6bff64577035bd7
Burak Yunkul <byunkul23@hotmail.com>
2021-09-09 16:35:16 +0300

    Result yapısı generic hale getirildi.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,14 +11,14 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
-			//ProductTest();
+			ProductTest();
 			//CategoryTest();
-			ProductManager productManager = new ProductManager(new EfProductDal());
+			//ProductManager productManager = new ProductManager(new EfProductDal());
 
-			foreach (var product in productManager.GetProductsDetails())
-			{
-				Console.WriteLine(product.ProductName + "/"+ product.CategoryName);
-			}
+			//foreach (var product in productManager.GetProductsDetails().Data)
+			//{
+			//	Console.WriteLine(product.ProductName + "/"+ product.CategoryName);
+			//}
 		}
 
 		private static void CategoryTest()
@@ -34,10 +34,20 @@ namespace ConsoleUI
 		{
 			ProductManager productManager = new ProductManager(new EfProductDal());
 
-			foreach (var product in productManager.GetByUnitPrice(50, 100))
+			var result = productManager.GetProductsDetails();
+			if (result.Success==true)
 			{
-				Console.WriteLine(product.ProductName);
+				foreach (var product in result.Data)
+				{
+						Console.WriteLine(product.ProductName + "/"+ product.CategoryName);
+				}
 			}
+			else
+			{
+				Console.WriteLine(result.Message);
+			}
+
+			
 		}
 	}
 }
